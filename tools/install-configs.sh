8742dcdac00a4c0e3c18c3c3fffdbdce65347dd4
mirmik <mirmikns@yandex.ru>
2022-07-31 02:10:02 +0300

    'a'

--- tools/install-configs.sh
+++ tools/install-configs.sh
@@ -1,5 +1,5 @@
 mkdir -p /etc/rfdaemon/
 mkdir -p /etc/reproxy/
 
-cp configs/applications.conf /etc/rfdaemon/applications.conf
+cp configs/applications.conf /etc/rfdaemon/applications.json
 cp configs/reproxy.yml /etc/reproxy.yml
